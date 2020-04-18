b774f0d7a9bc5cd39c8cbf7aecce2c0f9553edb9
肖珍雷 <2369941859@qq.com>
2020-04-18 17:31:23 +0800

    first

---
+++ src/components/blank.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<a v-if="msg.url" :href="msg.url" target="_blank" :style="msg.style">{{ msg.label }}</a>
+		<span v-else :href="msg.url" :style="msg.style">{{ msg.label }}</span>
+	</div>
+</template>
+<script>
+export default {
+	// mounted() {
+	// 	this.$post(this.$API.URL_GET_USER_INFO, {}, '')
+	// }
+	data() {
+		return {
+			msg: systomConfig
+		}
+	}
+}
+</script>
